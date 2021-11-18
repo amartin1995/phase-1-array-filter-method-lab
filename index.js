1d044522b8a78734dbf60a9520662a83148136bc
Alex Martin <alex.pmartin1@gmail.com>
2021-11-18 12:59:43 -0500

    commit tree 2e626fef3c09639eebd950f9770d7f12aebd1f14

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+// const findMatching = drivers => {
+//      let name = drivers.filter(drivers => drivers.name === true)
+//      return drivers !== undefined ? drivers.name : undefined
+// }
+
+function findMatching(drivers, matchingName){
+    return drivers.filter(function(el){
+        return el.toLowerCase().indexOf(matchingName.toLowerCase()) !== -1;
+    })
+}
+
+
+
+function fuzzyMatch(drivers, similarName){
+    return drivers.filter(function(driverName){
+        console.log(`name`, driverName)
+        console.log(`-------`, similarName)
+        console.log(`return`, driverName.includes(similarName)) 
+        console.log(`heelllllooo`, similarName)
+        return driverName.includes(similarName)
+        
+    })
+}
+
+function matchName(driverObj, nameMatch){
+
+}
